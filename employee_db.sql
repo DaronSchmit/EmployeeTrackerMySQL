2ed610a4ff9a9bfaeedc6eba71e36ab58a9cfdbe
Daron Schmit <darons23@gmail.com>
2021-03-19 15:55:01 -0500

    added foreign key for roles

--- employee_db.sql
+++ employee_db.sql
@@ -24,6 +24,9 @@ CREATE TABLE roles (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
+  FOREIGN KEY(department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
   PRIMARY KEY (id)
 );
 
