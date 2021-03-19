6dc4a4fc36e4e74f813d3970d5dde2e95aff5430
Daron Schmit <darons23@gmail.com>
2021-03-19 16:33:50 -0500

    fixed seeding

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,15 +13,17 @@ INSERT INTO roles (title, salary, department_id)
                   ('HR Lead', '125000', 3),
                   ('Legal Consultant', '250000', 4),
                   ('CEO', '1000000000', 4);
-       
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-       VALUES        ('Little', 'Dipper', 1, 3),
-	                 ('Ursa', 'Minor', 1, 3),
-                     ('Big', 'Dipper', 3, 4),
-                     ('Star', 'Lord', 4, 10),
-                     ('Mini', 'Money', 2, 7),
-                     ('Other', 'Money', 2, 7),
-                     ('Big', 'Money', 5, 10),
-                     ('Dont do', 'anything dumb', 6, 10)
-                     ('You did', 'something dumb', 7, 10)
-                     ('Big', 'Kahuna', 8, null);
+     VALUES          ('Big', 'Kahuna', 8, null),
+                     ('Star', 'Lord', 4, 1),
+                     ('Big', 'Money', 5, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      VALUES         ('Dont do', 'anything dumb', 6, 1),
+                     ('You did', 'something dumb', 7, 1),
+                     ('Little', 'Dipper', 1, 2),
+	                 ('Ursa', 'Minor', 1, 2),
+                     ('Big', 'Dipper', 3, 2),
+                     ('Mini', 'Money', 2, 3),
+                     ('Other', 'Money', 2, 3);
