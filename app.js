2bbdafc9bd6024017556f39d447a0c360d5d84a9
Daron Schmit <darons23@gmail.com>
2021-03-19 16:42:01 -0500

    added package dependencies

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const inquirer = require("inquirer");
-const connection = require("./mysql-connection/connection");
+const connection = require("./connection.js");
 const cTable = require("console.table");
 const logo = require("asciiart-logo");
 const config = require("./package.json");
+
+let menu = async () =>
+{   
+    const menu = await inquirer.prompt(
+        [{
+            type: "list",
+            message: "What would you like to do? ",
+            name: "choice",
+            choices: [
+                'View all employees',
+                'View all departments',
+                'View all roles',
+                'Add employee',
+                'Update employee role',
+                'Add department',
+                'Add role',
+                'Exit'
+            ]
+        }]);
+    }
